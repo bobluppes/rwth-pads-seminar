95b6735f96a6da7d4f0d805014579e0af8e57287
Nick Weber <n.weber@celonis.com>
2023-06-06 10:28:00 +0200

    Formatting

--- main.cpp
+++ main.cpp
@@ -4,35 +4,7 @@
 #include <iostream>
 #include <algorithm>
 
-enum alphabet {
- A = 0,
- B = 1,
- C = 2,
- D = 3,
- E = 4,
- F = 5,
- G = 6,
- H = 7,
- I = 8,
- J = 9,
- K = 10,
- L = 11,
- M = 12,
- N = 13,
- O = 14,
- P = 15,
- Q = 16,
- R = 17,
- S = 18,
- T = 19,
- U = 20,
- V = 21,
- W = 22,
- X = 23,
- Y = 24,
- Z = 25,
- END = 26	 
-};
+enum alphabet { A = 0, B = 1, C = 2, D = 3, E = 4, F = 5, G = 6, H = 7, I = 8, J = 9, K = 10, L = 11, M = 12, N = 13, O = 14, P = 15, Q = 16, R = 17, S = 18, T = 19, U = 20, V = 21, W = 22, X = 23, Y = 24, Z = 25, END = 26 };
 
 [[nodiscard]] int to_code(const alphabet a) { return static_cast<int>(a); }
 [[nodiscard]] alphabet to_alphabet(const int c) { 
